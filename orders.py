d37643ae5c2354d528b84ba155252cbbe1830493
Brendan DeLeeuw <test>
2023-03-26 11:42:47 -0600

    Discovered and playing around with TypeGuards to narrow generic typevars

--- orders.py
+++ orders.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Generic, TypeVar, Union, Callable, List, Tuple
+from typing import Generic, TypeVar, Union, Callable, List, Tuple, TypeGuard, Type
 
 A = TypeVar("A")
 B = TypeVar("B")
@@ -7,32 +7,32 @@ S = TypeVar("S")
 P = TypeVar("P")
 I = TypeVar("I", bound=int)
 
-@dataclass
-class Left(Generic[A]):
-    left: A
-
-@dataclass
-class Right(Generic[A]):
-    right: A
-
-Either = Union[Left[A], Right[B]]
 
 class Trivial(Generic[A]):
     pass
 
-class Natural(Generic[A]):
-    def __new__(cls, n: I) -> "Natural[I]":
-        inst = super(Natural, cls).__new__(cls)
-        inst.n = n  # type: ignore[attr-defined]
-        return inst  # type: ignore[return-value]
 
-class Sum(Generic[S]):
-    def __new__(cls, left: "Order[A]", right: "Order[B]") -> "Sum[Either[A, B]]":
-        inst = super(Sum, cls).__new__(cls)
-        # init done here because adding __init__ causes the return type of __new__ to be ignored/overwritten
-        inst.left = left  # type: ignore[attr-defined]
-        inst.right = right  # type: ignore[attr-defined]
-        return inst  # type: ignore[return-value]
+class Natural(Generic[A]):
+    def __new__(cls: "Type[Natural[int]]", n: int) -> "Natural[int]":
+        return super(Natural, cls).__new__(cls)
+    def __init__(self: "Natural[int]", n: int):
+        self.n = n
+
+def is_nat_int(o: Natural[A]) -> TypeGuard[Natural[int]]:
+    if issubclass(type(o.n), int):
+        return True
+    raise ValueError(f"How did you create Natural {o} not of type Natural[int]?")
+
+@dataclass
+class _NatList(Generic[A, B]):
+    # I think I need a more generic OrdList class that is just used to guard
+    # the order and list simultaneously
+    o: Natural[A]
+    xs: List[Tuple[A, B]]
+
+    @staticmethod
+    def is_valid_nat(nl: "_NatList[A, B]") -> "TypeGuard[_NatList[int, B]]":
+        return is_nat_int(nl.o)
 
 class Product(Generic[S]):
     def __new__(cls, fst: "Order[A]", snd: "Order[B]") -> "Product[Tuple[A, B]]":
@@ -51,10 +51,10 @@ class Map(Generic[S]):
         inst.target = target  # type: ignore[attr-defined]
         return inst  # type: ignore[return-value]
 
+
 Order = Union[
     Trivial[A],
     Natural[A],
-    Sum[A],
     Product[A],
     Map[A],
 ]
@@ -74,11 +74,7 @@ def _split(i: int) -> Tuple[int, int]:
 # first 16 bits are grouped into the negative numbers and the second half are the positive numbers
 # I think is the representation meant here.
 ordInt32 = Map(_split, Product(ordNat16, ordNat16))
-
-def _to_either(b: bool) -> Either[None, None]:
-    return Right(None) if b else Left(None)
-
-ordBool = Map(_to_either, Sum(ordUnit, ordUnit))
+reveal_type(ordInt32)
 
 def _mod_2(i: int) -> int:
     return i % 2
@@ -92,35 +88,29 @@ def refine(r1: Order[A], r2: Order[A]) -> Order[A]:
     """Creates an Order that does r1 first then r2"""
     return Map(_tuple, Product(r1, r2))
 
+
 def sdisc(o: Order[A]) -> Disc[A, B]:
     def res(xs: List[Tuple[A, B]]) -> List[List[B]]:
         reveal_type(o)
         if len(xs) == 0:
             return []
-        if len(xs) == 1:
+        elif len(xs) == 1:
             return [[xs[0][1]]]
-        if isinstance(o, Trivial):
-            reveal_type(o)
+        elif isinstance(o, Trivial):
             return [[x[1] for x in xs]]
-        if type(o) is Natural:
-            reveal_type(o)
-            reveal_type(xs)
-            res = [[] for i in range(o.n)]
-            for k, v in xs:
-                res[k].append(v)
-            return list(filter(lambda x: len(x) != 0, res))
-        if type(o) is Sum:
-            reveal_type(o)
-            lefts = []
-            rights = []
-            for k, v in xs:
-                if type(k) is Left:
-                    lefts.append((k.left, v))
-                if type(k) is Right:
-                    rights.append((k.right, v))
-            return sdisc(o.left)(lefts) + sdisc(o.right)(rights)
+        elif isinstance(o, Natural):
+            nl = _NatList(o, xs)
+            if nl.is_valid_nat(nl):
+                o = nl.o
+                rxs = nl.xs
+                reveal_type(o)
+                reveal_type(rxs)
+                res = [[] for i in range(o.n)]
+                for k, v in rxs:
+                    res[k].append(v)
+                    return list(filter(lambda x: len(x) != 0, res))
         """
-        if type(o) is Product:
+        elif type(o) is Product:
             ys = []
             for k, v in xs:
                 # Reorder xs so the key is just the first pair of the product
@@ -130,7 +120,7 @@ def sdisc(o: Order[A]) -> Disc[A, B]:
             for y in sdics(o.fst)(ys):
                 res.extend(sdics(o.snd)(y))
             return res
-        if type(o) is Map:
+        elif type(o) is Map:
             reveal_type(o)
             return sdisc(o.target)([(o.f(k), v) for k, v in xs])
         """
