713d930368fa70bb1385ffc10a007de0532c1f42
Brendan DeLeeuw <test>
2023-04-02 16:24:30 -0600

    Breaking up the file a little

--- discer/relations.py
+++ discer/relations.py
@@ -1,4 +1,4 @@
-""" Class for representations of relations over another type.
+"""Representation Types of relations over another type, common representation, and representation constructors.
 
 The classes are not relations themselves but are structures denoting/representing a relation over another type..
 Look at section 5 and 6 of the paper for more information.
@@ -10,7 +10,6 @@ for a relation of any arity.
 """
 
 from dataclasses import dataclass
-from itertools import chain
 from typing import (
     Generic,
     TypeVar,
@@ -157,92 +156,16 @@ ordNat8 = Natural(255)
 ordNat16 = Natural(655535)
 
 
-def _split(i: int) -> Tuple[int, int]:
-    i -= 2147483648
-    return (i >> 16) & 655535, i & 655535
-
-
-# 32 bit signed its are a product of two 16 bit natural numbers where the
-# first 16 bits are grouped into the negative numbers and the second half are the positive numbers
-# I think is the representation meant here.
-ordInt32 = Map(_split, ProductL(ordNat16, ordNat16))
-
-
-def _mod_2(i: int) -> int:
-    return i % 2
-
-
-evenOdd = Map(_mod_2, Natural(1))
-
-
 def _tuple(x: A) -> Tuple[A, A]:
     return (x, x)
 
 
 def refine(r1: Relation[A], r2: Relation[A]) -> Relation[A]:
-    """Creates an Order on A that does r1 first then r2"""
+    """Creates a Relation on A that does r1 first then r2"""
     return Map(_tuple, ProductL(r1, r2))
 
-
-def sdisc(o: Relation[A], xs: List[Tuple[A, V]]) -> List[List[V]]:
-    """Order a -> [(a, v)] -> [[v]]"""
-    rl = _RelList(o, xs)
-    if len(xs) == 0:
-        return []
-    elif len(xs) == 1:
-        return [[xs[0][1]]]
-    elif rl.is_trivial(rl):
-        return [[x[1] for x in xs]]
-    elif rl.is_natural(rl):
-        res: List[List[V]] = [[] for i in range(rl.relation.n + 1)]
-        for k, v in rl.xs:
-            res[k].append(v)
-        return list(filter(lambda x: len(x) != 0, res))
-    elif rl.is_sum(rl):
-        lefts = []
-        rights = []
-        for e, v in rl.xs:
-            if isinstance(e, Left):
-                lefts.append((e.left, v))
-            else:
-                rights.append((e.right, v))
-        return sdisc(rl.relation.left, lefts) + sdisc(rl.relation.right, rights)
-    elif rl.is_product(rl):
-        ys = []
-        for kp, v in rl.xs:
-            k1, k2 = kp
-            ys.append((k1, (k2, v)))
-        res = []
-        for y in sdisc(rl.relation.fst, ys):
-            res.extend(sdisc(rl.relation.snd, y))
-        return res
-    elif rl.is_map(rl):
-        mapped = [(rl.relation.f(k), v) for k, v in rl.xs]
-        return sdisc(rl.relation.source, mapped)
-    raise ValueError(f"Unknown Order {type(o)}")
-
-
-def sorted_partition(o: Relation[A], xs: List[A]) -> List[List[A]]:
-    return sdisc(o, [(x, x) for x in xs])
-
-
-def sort(o: Relation[A], xs: List[A]) -> List[A]:
-    return list(chain.from_iterable(sorted_partition(o, xs)))
-
-
-def test(max_=1000, len_=1000, iters=1000):
-    import time
-    import random
-
-    standard = 0
-    new = 0
-    for _ in range(iters):
-        l = [random.randint(0, max_) for _ in range(len_)]
-        start = time.time_ns()
-        o = sorted(l, key=lambda x: x % 2)
-        standard += time.time_ns() - start
-        start = time.time_ns()
-        n = sort(evenOdd, l)
-        new += time.time_ns() - start
-        assert o == n
-    return standard, new
+"""
+def infer(t: Type[A]) -> Relation[A]:
+    # todo: attempt to infer a relation on type A
+    # types that might be easy to infer tuples, named dicts, dataclasses
+"""
