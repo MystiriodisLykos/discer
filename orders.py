b123d7c09c3d3562d59096863342d09bb296b08b
Brendan DeLeeuw <brendandeleeuw@gmail.com>
2023-04-02 10:50:46 -0600

    Playing around with an alternate implementation
    Instead of unioning the types post construction playing around with them getting inherited by a root protocol type

--- orders.py
+++ orders.py
@@ -4,85 +4,58 @@ from typing import Generic, TypeVar, Union, Callable, List, Tuple, TypeGuard, Ty
 
 A = TypeVar("A")
 B = TypeVar("B")
-F = TypeVar("F")
-S = TypeVar("S")
-P = TypeVar("P")
+C = TypeVar("C", covariant=True)
 V = TypeVar("V")
-O = TypeVar("O", bound="Order")
-I = TypeVar("I", bound=int)
 
+class Relation(Protocol[C]):
+    ...
 
-class Trivial(Generic[A]):
+@dataclass
+class Trivial(Relation[A]):
     pass
 
 
-class Natural(Generic[A]):
-    def __new__(cls: "Type[Natural[int]]", n: int) -> "Natural[int]":
-        return super(Natural, cls).__new__(cls)
-    def __init__(self: "Natural[int]", n: int):
-        self.n = n
-
-
-class Product(Generic[S]):
-    def __new__(cls, fst: "Order[A]", snd: "Order[B]") -> "Product[Tuple[A, B]]":
-        inst = super(Product, cls).__new__(cls)
-        # init done here because adding __init__ causes the return type of __new__ to be ignored/overwritten
-        # Types for fst and snd are added through properties
-        inst._fst = fst # type: ignore[attr-defined]
-        inst._snd = snd # type: ignore[attr-defined]
-        return inst # type: ignore[return-value]
-
-    @property
-    def fst(self: "Product[Tuple[A, B]]") -> "Order[A]":
-        return self._fst # type: ignore[attr-defined]
-    
-    @property
-    def snd(self: "Product[Tuple[A, B]]") -> "Order[B]":
-        return self._snd # type: ignore[attr-defined]
-
-
-class Map(Generic[S]):
-    # This implemntation isn't perfect because the precise type of B gets lost.
-    # Adding a second type variable just to keep track of it ends up causing a lot (possibly infinite) type
-    # data to be inlcuded in Map and other Orders that is not super relavent to the Order itself.
-    # See <#todo> for more info.
-    def __init__(self: "Map[S]", f: Callable[[S], B], target: "Order[B]"):
-        self.f = f
-        self.target = target
-
-Order = Union[
-    Trivial[A],
-    Natural[A],
-    Product[A],
-    Map[A],
-]
-
+@dataclass
+class Natural(Relation[int]):
+    n: int
 
 @dataclass
-class _OrdList(Generic[O, A, V]):
-    """ Pair of an Order and key/pair List to be descrimiated
-    Used internally in disc to provide a TypeGuard against the Order and List simultaneously
+class Product(Relation[Tuple[A, B]]):
+    fst: Relation[A]
+    snd: Relation[B]
+
+@dataclass
+class Map(Relation[A], Generic[A, B]):
+    f: Callable[[A], B]
+    source: Relation[B]
+
+'''
+@dataclass
+class _RelList(Generic[O, A, V]):
+    """ Pair of a Relation and key/pair List to be descrimiated
+    Used internally to provide a TypeGuard against the Relation and List simultaneously
     """
     o: O
     xs: List[Tuple[A, V]]
 
     @staticmethod
-    def is_trivial(ol: "_OrdList[O, A, V]") -> "TypeGuard[_OrdList[Trivial[A], A, V]]":
+    def is_trivial(ol: "_RelList[O, A, V]") -> "TypeGuard[_RelList[Trivial[A], A, V]]":
         return isinstance(ol.o, Trivial)
 
     @staticmethod
-    def is_natural(ol: "_OrdList[O, A, V]") -> "TypeGuard[_OrdList[Natural[int], int, V]]":
+    def is_natural(ol: "_RelList[O, A, V]") -> "TypeGuard[_RelList[Natural[int], int, V]]":
         return isinstance(ol.o, Natural) and isinstance(ol.o.n, int)
 
     @staticmethod
-    def is_product(ol: "_OrdList[O, A, V]") -> "TypeGuard[_OrdList[Product[Tuple[F, S]], Tuple[F, S], V]]":
+    def is_product(ol: "_RelList[O, A, V]") -> "TypeGuard[_RelList[Product[Tuple[F, S]], Tuple[F, S], V]]":
         return isinstance(ol.o, Product)
 
     @staticmethod
-    def is_map(ol: "_OrdList[O, A, V]") -> "TypeGuard[_OrdList[Map[A], A, V]]":
+    def is_map(ol: "_RelList[O, A, V]") -> "TypeGuard[_RelList[Map[A], A, V]]":
         return isinstance(ol.o, Map)
+'''
     
-ordUnit: Trivial[None] = Trivial()
+ordUnit = Trivial[None]()
 
 ordNat8  = Natural(255)
 ordNat16 = Natural(655535)
@@ -104,25 +77,30 @@ evenOdd = Map(_mod_2, Natural(1))
 def _tuple(x: A) -> Tuple[A, A]:
     return (x, x)
 
-def refine(r1: Order[A], r2: Order[A]) -> Order[A]:
+def refine(r1: Relation[A], r2: Relation[A]) -> Relation[A]:
     """Creates an Order on A that does r1 first then r2"""
     return Map(_tuple, Product(r1, r2))
 
 
-def sdisc(o: Order[A], xs: List[Tuple[A, B]]) -> List[List[B]]:
+def sdisc(o: Relation[A], xs: List[Tuple[A, B]]) -> List[List[B]]:
         """Order a -> [(a, b)] -> [[b]]"""
-        ol = _OrdList(o, xs)
+        reveal_type(o)
         if len(xs) == 0:
             return []
         elif len(xs) == 1:
             return [[xs[0][1]]]
-        elif ol.is_trivial(ol):
-            return [[x[1] for x in ol.xs]]
-        elif ol.is_natural(ol):
+        elif isinstance(o, Trivial):
+            reveal_type(o)
+            reveal_type(xs)
+            return [[x[1] for x in xs]]
+        elif isinstance(o, Natural):
+            reveal_type(o)
+            reveal_type(xs)
             res: List[List[B]] = [[] for i in range(ol.o.n+1)]
             for k, v in ol.xs:
                 res[k].append(v)
             return list(filter(lambda x: len(x) != 0, res))
+        """
         elif ol.is_product(ol):
             ys = []
             for kp, v in ol.xs:
@@ -136,8 +114,10 @@ def sdisc(o: Order[A], xs: List[Tuple[A, B]]) -> List[List[B]]:
         elif ol.is_map(ol):
             mapped = [(ol.o.f(k), v) for k, v in ol.xs]
             return sdisc(ol.o.target, mapped)
+        """
         raise ValueError(f"Unknown Order {type(o)}")
 
+"""
 def sorted_partition(o: Order[A], xs: List[A]) -> List[List[A]]:
     return sdisc(o, [(x,x) for x in xs])
 
@@ -159,3 +139,5 @@ def test(max_=1000, len_=1000, iters=1000):
         new += (time.time_ns() - start)
         assert o == n
     return standard, new
+"""
+
